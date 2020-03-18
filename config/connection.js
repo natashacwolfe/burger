309a44ed6b4f5ba650f5fd53d874ba0a3d25a1ab
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-03-18 13:29:23 -0700

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 // dependencies 
 var mysql = require('mysql');
+var connection;
 
 
 // create connection
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.SQL_PW,
-  database: 'burgers_db'
-})
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+  });
+};
+
+
 // Make connection.
 connection.connect(function (err) {
   if (err) {
